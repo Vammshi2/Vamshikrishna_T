4ab0ad11a8c46b442b880ffc4bf184c085914e60
Vamshikrishna <162019269+Vammshi2@users.noreply.github.com>
2025-04-19 11:14:59 +0530

    Update Program-1.py

--- Problem-2/Program-1.py
+++ Problem-2/Program-1.py
@@ -1,1 +1,8 @@
+def generate_odd_series(n):
+    result = []
+    for i in range(n):
+        result.append(2 * i + 1)
+    return result
 
+a = int(input("Enter a number: "))
+print(", ".join(map(str, generate_odd_series(a))))

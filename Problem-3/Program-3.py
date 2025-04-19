50585f839568f70784f1bcb556648392d6d40ff1
Vamshikrishna <162019269+Vammshi2@users.noreply.github.com>
2025-04-19 11:22:15 +0530

    Update Program-3.py

--- Problem-3/Program-3.py
+++ Problem-3/Program-3.py
@@ -1,1 +1,9 @@
+def generate_odd_stack(a):
+    stack = []
+    limit = a if a % 2 == 1 else a - 1
+    for i in range(1, limit + 1, 2):
+        stack.append(i)
+    return stack[::-1][::-1]  # or simply return stack
 
+a = int(input("Enter a number: "))
+print(", ".join(map(str, generate_odd_stack(a))))

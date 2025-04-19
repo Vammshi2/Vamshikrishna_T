02453d0fecc472428080cbb210310fde927ba1d9
Vamshikrishna <162019269+Vammshi2@users.noreply.github.com>
2025-04-19 11:15:14 +0530

    Update Program-2.py

--- Problem-2/Program-2.py
+++ Problem-2/Program-2.py
@@ -1,1 +1,11 @@
+def generate_odd_recursive(n, i=0, result=None):
+    if result is None:
+        result = []
+    if i == n:
+        return result
+    result.append(2 * i + 1)
+    return generate_odd_recursive(n, i + 1, result)
+
+a = int(input("Enter a number: "))
+print(", ".join(map(str, generate_odd_recursive(a))))
 

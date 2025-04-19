926dc8b44f02660944cfdb861ccd28080102ec55
Vamshikrishna <162019269+Vammshi2@users.noreply.github.com>
2025-04-19 11:25:40 +0530

    Update Program-3.py

--- Problem-4/Program-3.py
+++ Problem-4/Program-3.py
@@ -1,1 +1,6 @@
+def count_multiples_functional(nums):
+    return {i: sum(map(lambda x: x % i == 0, nums)) for i in range(1, 10)}
+
+input_list = [1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30]
+print(count_multiples_functional(input_list))
 

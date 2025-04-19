37b08f24de5f8e4e3ffb93be79f5b49dd109cd9a
Vamshikrishna <162019269+Vammshi2@users.noreply.github.com>
2025-04-19 11:15:33 +0530

    Update Program-3.py

--- Problem-2/Program-3.py
+++ Problem-2/Program-3.py
@@ -1,1 +1,10 @@
+from collections import deque
 
+def generate_odd_with_queue(n):
+    q = deque()
+    for i in range(n):
+        q.append(2 * i + 1)
+    return list(q)
+
+a = int(input("Enter a number: "))
+print(", ".join(map(str, generate_odd_with_queue(a))))

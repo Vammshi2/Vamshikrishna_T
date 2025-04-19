1af69eea443b963832e9b080a1229e79a7e31138
Vamshikrishna <162019269+Vammshi2@users.noreply.github.com>
2025-04-19 11:25:09 +0530

    Update Program-1.py

--- Problem-4/Program-1.py
+++ Problem-4/Program-1.py
@@ -1,10 +1,12 @@
-def count_multiples(input_list):
+def count_multiples_basic(nums):
     result = {}
     for i in range(1, 10):
-        count = sum(1 for num in input_list if num % i == 0)
+        count = 0
+        for num in nums:
+            if num % i == 0:
+                count += 1
         result[i] = count
     return result
 
 input_list = [1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30]
-output = count_multiples(input_list)
-print(output)
+print(count_multiples_basic(input_list))

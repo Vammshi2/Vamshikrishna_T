9c3c61041100df0602861616df283af073c45490
Vamshikrishna <162019269+Vammshi2@users.noreply.github.com>
2025-04-19 11:21:24 +0530

    Update Program-1.py

--- Problem-3/Program-1.py
+++ Problem-3/Program-1.py
@@ -1,1 +1,10 @@
+def generate_odd_upto_a(a):
+    if a % 2 == 0:
+        a -= 1
+    result = []
+    for i in range(1, a + 1, 2):
+        result.append(i)
+    return result
 
+a = int(input("Enter a number: "))
+print(", ".join(map(str, generate_odd_upto_a(a))))
